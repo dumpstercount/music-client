095e7993322ef53b8ff962d099329593f040f9fd
dumpstercount <hello2022schweiz@gmail.com>
2025-02-07 15:33:04 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MusicButton from "./MusicButton";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl mt-4">Multiplayer Music</h1>
+      <MusicButton />
+    </div>
+  );
+}
+
+export default App;
+
